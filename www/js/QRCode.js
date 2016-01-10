204f2249e6c8701510df139d4064ccb5f39ae0bf
Steve Rubinstein <slrubinstein@gmail.com>
2016-01-10 18:39:42 -0500

    add cordova barcode reader

--- www/js/QRCode.js
+++ www/js/QRCode.js
@@ -2,11 +2,20 @@ angular.module('bizCard')
 .factory("QRCode", function() {
 
   return {
-  	createQR: createQR
+  	createQR: createQR,
+  	scanQR: scanQR
   };
 
   function createQR(id, text) {
   	new QRCode(document.getElementById(id), text);
+  }
+
+  function scanQR(successCallback, errorCallback) {
+  	try {
+    	cordova.plugins.barcodeScanner.scan(successCallback, errorCallback);
+  	} catch(e) {
+  		console.log(e);
+  	}
   }
 
 });
--- www/js/TradeController.js
+++ www/js/TradeController.js
@@ -5,12 +5,23 @@ angular.module('bizCard')
   var vm = this;
 
   vm.authData;
+  vm.message = '';
+  vm.scan = scan;
   
   init();
 
   function init() {
 		vm.authData = User.getUser();
   	QRCode.createQR('qr-code', vm.authData.uid);
+  }
+
+  function scan() {
+  	QRCode.scanQR(function(result) {
+  		vm.message = result.text;
+  	}, function(e) {
+  		console.log(e);
+  		vm.message = e;
+  	});
   }
 
 });
